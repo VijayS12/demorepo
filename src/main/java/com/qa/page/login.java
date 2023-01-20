5fd6580fed32725da87cce8dd0331a0a6b40f1b2
vijay sonulkar <you@example.comvijay.sonulkar@gmail.com>
2023-01-20 12:14:59 +0530

    added login and search page

---
+++ src/main/java/com/qa/page/login.java
@@ -0,0 +1,10 @@
+package com.qa.page;
+
+public class login {
+public void loginset() {
+	System.out.println("this is login");
+}
+public void loginsetting() {
+	System.out.println("this is login setting");
+}
+}
---
+++ src/main/java/com/qa/page/searchPage.java
@@ -0,0 +1,7 @@
+package com.qa.page;
+
+public class searchPage {
+public void searchweb() {
+	System.out.println("this is search web");
+}
+}
